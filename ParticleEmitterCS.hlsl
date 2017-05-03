ffc0ab79dc24081b537854375206164d6ecaaa6d
asr3784 <asr3784@IGMOH1370-22>
2017-05-03 17:43:11 -0400

    Changed the randomness of position for particle generation

--- ParticleEmitterCS.hlsl
+++ ParticleEmitterCS.hlsl
@@ -13,7 +13,9 @@ void main(uint3 DTid : SV_DispatchThreadID)
 	{
 		uint pid = deadList.Consume();
 
-		particles[pid].position = float4(snoise(float2(position.x, totalTime)), snoise(float2(position.y, totalTime)), snoise(float2(position.z, totalTime)), position.w);
+		particles[pid].position = position;
+		particles[pid].position.x += (curlNoise3D(particles[pid].position.xyz, totalTime).x % 10) / 100;
+		particles[pid].position.z += (curlNoise3D(particles[pid].position.xyz, totalTime).z % 10) / 100;
 		particles[pid].velocity = velocity;
 	}
 }
