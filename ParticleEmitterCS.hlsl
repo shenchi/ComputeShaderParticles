33412683cb62f9e3d7006ca0ccf16937a36be6e7
asr3784 <asr3784@IGMOH1370-22>
2017-05-03 18:00:22 -0400

    Cleaned up a little bit of code

--- ParticleEmitterCS.hlsl
+++ ParticleEmitterCS.hlsl
@@ -14,8 +14,10 @@ void main(uint3 DTid : SV_DispatchThreadID)
 		uint pid = deadList.Consume();
 
 		particles[pid].position = position;
-		particles[pid].position.x += (curlNoise3D(particles[pid].position.xyz, totalTime).x % 10) / 100;
-		particles[pid].position.z += (curlNoise3D(particles[pid].position.xyz, totalTime).z % 10) / 100;
+
+		float3 randomFloat = curlNoise3D(particles[pid].position.xyz, totalTime);
+		particles[pid].position.xz += (randomFloat.xz % 10) / 100;
+
 		particles[pid].velocity = velocity;
 	}
 }
